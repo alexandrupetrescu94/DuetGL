bde3d3616d1e64e6dcc648af52390971526aeadf
AlexP <alexandrupetrescu94@yahoo.com>
2015-04-18 15:43:00 +0300

    changed rectangle to circle

--- DuetGL/DuetGL.cpp
+++ DuetGL/DuetGL.cpp
@@ -1,13 +1,42 @@
-#include<windows.h>
+#include <windows.h>
 #include <GL/freeglut.h>
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
 using namespace std;
 
+#define PI (3.141592653589793)
 static GLdouble i = 600.0;
 static GLdouble duet_ball_angle = 0.0;
 static GLdouble alpha = 1.0;
+
+typedef struct
+{
+	int x_center;
+	int y_center;
+	int radius;
+} ball;
+
+ball red_ball; //20 20 30 30
+ball blue_ball; //-30 -30 -20 -20
+
+void create_ball(int x, int y, int radius, ball &b) {
+	b.x_center = x;
+	b.y_center = y;
+	b.radius = radius;
+
+	int triangleAmount = 20; //# of triangles used to draw circle
+	GLfloat twicePi = 2.0f * PI;
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x, y); // center of circle
+	for (int point = 0; point <= triangleAmount; point++) {
+		glVertex2f(
+			x + (radius * cos(point *  twicePi / triangleAmount)),
+			y + (radius * sin(point * twicePi / triangleAmount))
+			);
+	}
+	glEnd();
+}
 
 void init(void)
 {
@@ -15,27 +44,34 @@ void init(void)
 	gluOrtho2D(0.0, 800.0, 0.0, 600.0);
 	glShadeModel(GL_FLAT);
 }
-void dreptunghi(void)
+
+void scena(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	//Red_ball
 	glPushMatrix();
 	glTranslated(350.0, 250.0, 0.0);
 	glPushMatrix();
 	glRotated(duet_ball_angle, 0.0, 0.0, 1.0);
 	glColor3f(1.0, 0.0, 0.0);
-	glRecti(20, 20, 30, 30);
+	//glRecti(20, 20, 30, 30); //Initial recti, but changed to ball 
+	create_ball(25, 25, 5, red_ball);
 	glPopMatrix();
 	glPopMatrix();
 
+	//Blue_ball
 	glPushMatrix();
 	glTranslated(350.0, 250.0, 0.0);
 	glPushMatrix();
 	glRotated(duet_ball_angle, 0.0, 0.0, 1.0);
 	glColor3f(0.0, 0.0, 1.0);
-	glRecti(-30, -30, -20, -20);
+	//glRecti(-30, -30, -20, -20); //Initial recti, but changed to ball 
+	create_ball(-25, -25, 5, blue_ball);
 	glPopMatrix();
 	glPopMatrix();
 
+	//Falling Object
 	glPushMatrix();
 	glTranslated(375.0, i, 0.0);
 	glColor3f(1.0, 0.0, 1.0);
@@ -111,20 +147,15 @@ void mouse(int button, int state, int x, int y)
 	}
 }
 
-
-/*
-*  Request double buffer display mode.
-*  Register mouse input callback functions
-*/
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(800, 600);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Dreptunghi cu satelit");
+	glutCreateWindow("DuetGL");
 	init();
-	glutDisplayFunc(dreptunghi);
+	glutDisplayFunc(scena);
 	glutReshapeFunc(reshape);
 	glutMouseFunc(mouse);
 	glutMainLoop();
